efde7af801237298bcf5e8f0fc7ea8c5ab019f2b
Khadim Hussain <khadimkhan78655@gmail.com>
2024-03-04 22:19:10 +0500

    added docker support ngnix

--- animegen-backend/app/core/config.py
+++ animegen-backend/app/core/config.py
@@ -4,8 +4,16 @@ from datetime import timedelta
 SECRET_KEY = "supersecritkey"
 ALGORITHM = "HS256"
 TOKEN_EXP = timedelta(days=7)
-DATABASE_URL: str = "postgresql://postgres:root@localhost:5432/animegen"
-SERVER_URL: str = "http://192.168.18.19:8000"
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+DB_HOST = os.getenv("DB_HOST", "database")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "animegen")
+DATABASE_URL: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+SERVER_URL: str = "http://backend:8000"
+
 FILE_STORAGE_PATH: str = os.path.join(os.getcwd(), "image_store")
 if not os.path.exists(FILE_STORAGE_PATH):
     os.mkdir(FILE_STORAGE_PATH)
--- animegen-backend/app/main.py
+++ animegen-backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Update this with your frontend's actual URL in production
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
