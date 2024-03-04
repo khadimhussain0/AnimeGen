94e806eea383be55f2f1e4477eeeb28ced24cfa7
Khadim Hussain <khadimkhan78655@gmail.com>
2024-03-04 11:03:32 +0500

    initial commit

--- animegen-backend/app/core/config.py
+++ animegen-backend/app/core/config.py
@@ -5,7 +5,7 @@ SECRET_KEY = "supersecritkey"
 ALGORITHM = "HS256"
 TOKEN_EXP = timedelta(days=7)
 DATABASE_URL: str = "postgresql://postgres:root@localhost:5432/animegen"
-SERVER_URL: str = "http://localhost:8000"
+SERVER_URL: str = "http://192.168.18.19:8000"
 FILE_STORAGE_PATH: str = os.path.join(os.getcwd(), "image_store")
 if not os.path.exists(FILE_STORAGE_PATH):
     os.mkdir(FILE_STORAGE_PATH)
